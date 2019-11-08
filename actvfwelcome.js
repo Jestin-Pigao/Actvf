eeb7fb42dba3c94a105a09e295b85c1f5bdac5cc
Jestin-Pigao <57328062+Jestin-Pigao@users.noreply.github.com>
2019-11-08 16:06:25 -0500

    Second commit

---
+++ actvfwelcome.js
@@ -0,0 +1,10 @@
+// Select your button
+let button = document.querySelector(".ButtonOne");
+button = document.querySelector(".ButtonTwo");
+
+// Add a click event listener to make the button say a word.
+button.addEventListener("click", function() {
+	// Your procedure goes here.
+	alert("There you are");
+
+});
